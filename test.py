d94e8def21e518836da829f6bb3e07ebaed441a1
Neil W <neilkey88@outlook.com>
2022-02-10 03:09:41 +1000

    some stuff

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 print('heelo world')
 print("welcome to the matrix")
 
-<<<<<<< HEAD
 print("Yo neo!")
 print("knock knock")
 print("matreix has you")
-=======
 # here is some stuff for new branch
->>>>>>> new_version
+print("Some stuff from pc")
