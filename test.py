f2c3ca9cc0983f0688fa94b7d67946d9d98663f4
Neil W <neilkey88@outlook.com>
2022-02-10 03:31:39 +1000

    new stuff for new branchs

--- test.py
+++ test.py
@@ -4,10 +4,6 @@ print("welcome to the matrix")
 print("Yo neo!")
 print("knock knock")
 print("matreix has you")
-# here is some stuff for new branch
-<<<<<<< HEAD
-print("Some stuff from pc")
-=======
->>>>>>> new_version
-print("some stuff added on website")
->>>>>>> 9425ebe815f5a056f703675801e7bbe710fe6dd8
+
+#some stuff for new branch
+print("new stuff")
